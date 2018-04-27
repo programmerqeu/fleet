eeae1773e99c7e8ca1b972fd887224b74b0dc885
Andre Lademann <andre.lademann@netresearch.de>
2018-04-27 16:11:41 +0200

    Added image support to cli

--- ui.js
+++ ui.js
@@ -2,13 +2,18 @@
 
 const {h, Component, Text} = require('ink')
 const PropTypes = require('prop-types')
+const Image = require('ink-image');
 
 class UI extends Component {
 	render({name}) {
 		return (
-			<Text green>
-				I love {name}
-			</Text>
+			<div>
+				<Image preserveAspectRatio src='assets/Airplane.png' width='100px'/>
+				<br/>
+				<Text green>
+					I love {name}
+				</Text>
+			</div>
 		)
 	}
 }
